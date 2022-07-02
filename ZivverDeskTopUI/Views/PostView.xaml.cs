1eea6ddfc8f68d54e444bc10664d0e9cd2ae12d9
anthonyorji <anthony.orji@7peakssoftware>
2022-07-02 21:34:43 +0100

    Add project files.

---
+++ ZivverDeskTopUI/Views/PostView.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ZivverDeskTopUI.Views
+{
+    /// <summary>
+    /// Interaction logic for PostView.xaml
+    /// </summary>
+    public partial class PostView : Window
+    {
+        public PostView()
+        {
+            InitializeComponent();
+        }
+    }
+}
